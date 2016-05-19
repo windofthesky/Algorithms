244736cb721468c27884cb7532768d8d82103665
Hades.Yang <windofthesky@aliyun.com>
2016-05-19 08:50:56 +0800

    Update BinarySearch.java
    Add another method using recursion.

--- BinarySearch.java
+++ BinarySearch.java
@@ -24,8 +24,32 @@ public class BinarySearch
 				return middle;
 			}
 		}
-		
+
 		return -1;
+	}
+	
+	//Another method of recursion.
+	public static int BiSearch(int key, int[] a, int lo, int hi)
+	{
+		if(lo > hi)
+		{
+			return -1;
+		}
+		
+		int mid = lo + (hi-lo)/2;
+		if(key > a[mid])
+		{
+			return BiSearch(key, a, mid+1, hi);
+		}
+		else if(key < a[mid])
+		{
+			return BiSearch(key, a, lo, mid-1);
+		}
+		else
+		{
+			return mid;
+		}
+		
 	}
 	
 	//Test the BinarySearch function.
@@ -34,5 +58,7 @@ public class BinarySearch
 		int[] arraysort = {1,2,3,4,5,6,7,8,9,10};
 		int key = 9;
 		System.out.println("The key is :"+ key+ " and the index is : " + rank(key, arraysort));
+		System.out.println("The key is :"+ key+ " and the index is : " + BiSearch(key, arraysort, 0, arraysort.length-1));
+		System.out.println(arraysort[0]);
 	}
 }
